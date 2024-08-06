dc5f3ad080857bf783178e307d3b952cc5f56961
Lakshya Saxena <lakshya.saxena@loconav.com>
2024-08-06 19:50:26 +0530

    initial commit

---
+++ src/main/java/com/example/kafka/integration/models/KafkaResponse.java
@@ -0,0 +1,16 @@
+package com.example.kafka.integration.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class KafkaResponse {
+
+    private Boolean success;
+    private String message;
+}
